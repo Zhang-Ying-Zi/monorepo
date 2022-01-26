f907928a2645d5894c87faa82aabc96cbf324116
Molly2Molly <489573239@qq.com>
2022-01-26 14:38:07 +0800

    add rxjs-store

---
+++ packages/@rxjs:store/lib/index.d.ts
@@ -0,0 +1,1 @@
+export { Action, RxJsStore } from "./src/RxJsStore";
---
+++ packages/@rxjs:store/lib/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RxJsStore = void 0;
+var RxJsStore_1 = require("./src/RxJsStore");
+Object.defineProperty(exports, "RxJsStore", { enumerable: true, get: function () { return RxJsStore_1.RxJsStore; } });
