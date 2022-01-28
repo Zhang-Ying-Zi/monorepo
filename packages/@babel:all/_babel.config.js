472c5306a37a176d3b7a9887a1e1c520dfa5b0f7
Molly2Molly <489573239@qq.com>
2022-01-28 14:49:46 +0800

    update

---
+++ packages/@babel:all/_babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function (api) {
+  // Cached based on the value of some function. If this function returns a value different from
+  // a previously-encountered value, the plugins will re-evaluate.
+  var env = api.cache(() => process.env.NODE_ENV);
+
+  const plugins = ["@babel/proposal-class-properties", "@babel/proposal-object-rest-spread"];
+
+  const presets = [
+    [
+      "@babel/env",
+      {
+        targets: "> 0.25%, not dead",
+        useBuiltIns: "usage",
+        corejs: "3",
+        modules: "umd", //"amd" | "umd" | "systemjs" | "commonjs" | "cjs" | "auto" | false, defaults to "auto".
+      },
+    ],
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
+};
