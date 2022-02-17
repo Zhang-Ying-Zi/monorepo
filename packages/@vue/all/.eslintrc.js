8ee7c9444e907e6088c02e63debd87e03c9fa804
Molly2Molly <489573239@qq.com>
2022-02-17 16:35:24 +0800

    update

--- packages/@vue/all/.eslintrc.js
+++ packages/@vue/all/.eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
-  extends: [
-    // "eslint:recommended",
-    "plugin:vue/recommended",
-    "prettier",
-  ],
+  // extends: [
+  //   // "eslint:recommended",
+  //   "plugin:vue/recommended",
+  //   "prettier",
+  // ],
+  // 继承
+  extends: "../../../.eslintrc",
   rules: {},
 };
