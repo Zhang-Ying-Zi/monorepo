d374c7f9b87d1a5f5f18ef848957eb41d8cc36c9
Molly2Molly <489573239@qq.com>
2022-02-17 16:33:33 +0800

    update

--- packages/@babel/all/src/react-test.js
+++ packages/@babel/all/src/react-test.js
@@ -1,18 +1,20 @@
-export default React.createClass({
+import React from "react";
+
+export class Test extends React.Component {
   getInitialState() {
     return { num: this.getRandomNumber() };
-  },
+  }
 
   getRandomNumber() {
     return Math.ceil(Math.random() * 6);
-  },
+  }
 
   render() {
     return (
       <div>
-        Your dice roll:
+        Your dice roll is:
         {this.state.num}
       </div>
     );
-  },
-});
+  }
+}
